2b87a1bfd87261ae560e1cd6dce57185e21f66e0
Tanner Satchwell <tannersatch@gmail.com>
2017-02-02 16:14:46 -0700

    updated permissions, clean up

--- practice/energize.sh
+++ practice/energize.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "$SLURM_ARRAY_TASK_ID" > "$SLURM_ARRAY_JOB_ID.$SLURM_ARRAY_TASK_ID.out"
-echo "$RANDOM" >> "$SLURM_ARRAY_JOB_ID.$SLURM_ARRAY_TASK_ID.out"
+#SBATCH --ntasks=1 --mem=1G --time=00:00:04 -a 1-10
+
+echo "$SLURM_ARRAY_TASK_ID $RANDOM" > "$SLURM_ARRAY_JOB_ID.$SLURM_ARRAY_TASK_ID.out"
 
 exit 0
