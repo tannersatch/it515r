9227db24075418b7c0f8f08c5ccbbc1a44203e06
Tanner Satchwell <tannersatch@gmail.com>
2017-03-30 13:41:32 -0600

    simplified algorithm

--- threads.cpp
+++ threads.cpp
@@ -6,15 +6,13 @@
 #include <iostream>
 #include <cstdint>
 #include <cmath>
-#include <omp.h>
 
 using std::cout;
 using std::cin;
 using std::endl;
 
 void initializeGrid(uint32_t &r, uint32_t &c, float ** grid);
-bool isStable(uint32_t &r, uint32_t &c, float &e, float &er, float ** grid);
-void recalcGrid(uint32_t &r, uint32_t &c, float ** grid, float ** tmp);
+void swapGrid(uint32_t &r, uint32_t &c, float ** grid, float ** tmp);
 void printGrid(uint32_t &r, uint32_t &c, float ** grid);
 void cleanUp(uint32_t &r, float ** grid);
 
@@ -45,22 +43,29 @@ int main() {
 	// initialize grid
 	initializeGrid(rows, cols, grid1);
 
-	// make the program parallel
-	#pragma omp parallel
+	// check for stability and recalculate
+	do {
+		for (uint32_t i = 1; i < r-1; i++) {
+			for (uint32_t j = 1; j < c-1; j++) {
+				float average = ((grid1[i-1][j] + grid1[i+1][j] + grid1[i][j-1] + grid1[i][j+1])/4);
+				float value = grid1[i][j];
 
-	// check for stability, and recalc as needed
-	while (!isStable(rows, cols, epsilon, error, grid1) && itr < 400) {
-		recalcGrid(rows, cols, grid1, grid2);
-
-		#pragma omp barrier
-
-		#pragma omp single
-		{
-			itr ++;
+				grid2[i][j] = average;
+				float tmp_val = fabs(average-value);
+				error = (tmp_val > error) ? tmp_val : error;
+			}
 		}
 
-		#pragma omp barrier
-	}
+		if (error < epsilon) {
+			break;
+		} 
+
+		itr ++;
+		swapGrid(grid1, grid2);
+		error = 0.0f;
+
+	} while (true);
+
 
 	// human readable test output
 	// cout << "Iterator: " << itr << endl;
@@ -102,57 +107,17 @@ void initializeGrid(uint32_t &r, uint32_t &c, float ** grid) {
 }
 
 /**
- * Check to see if the 2D grid is stable.
+ * A single iteration through the 2D grid to swap from grid1 to grid2.
  * @param r
  *	The number of rows in the 2D grid
  * @param c
  *	The number of columns in the 2D grid
- * @param grid
- *	Dereferenced 2D grid to be evaluated if it is stable
- * @return
- *	True if stable, false if unstable
-**/
-bool isStable(uint32_t &r, uint32_t &c, float &e, float &er, float ** grid) {
-	float tmp_val;
-	er = 0.0f;
-	#pragma omp for reduction(max:er)
-	for (uint32_t i = 1; i < r-1; i++) {
-		for (uint32_t j = 1; j < c-1; j++) {
-			tmp_val = (grid[i-1][j] + grid[i+1][j] + grid[i][j-1] + grid[i][j+1]);
-			tmp_val = fabs((tmp_val/4) - grid[i][j]) ;
-			er = (tmp_val > er) ? tmp_val : er;
-		}
-	}
-
-	if (er > e) {
-		return false;
-	} else {
-		return true;
-	}
-}
-
-/**
- * A single iteration through the 2D grid to recalculated possible stable values.
- * @param r
- *	The number of rows in the 2D grid
- * @param c
- *	The number of columns in the 2D grid
- * @param e
- *	The value of epsilon
  * @param grid
  *	Dereferenced 2D grid to be recalculated
  * @param tmp
  *	Dereferenced 2D grid to temporarily hold the recalculated values
 **/
-void recalcGrid(uint32_t &r, uint32_t &c, float ** grid, float ** tmp) {
-	#pragma omp for
-	for (uint32_t i = 1; i < r-1; i++) {
-		for (uint32_t j = 1; j < c-1; j++) {
-			tmp[i][j] = ((grid[i-1][j] + grid[i+1][j] + grid[i][j-1] + grid[i][j+1])/4);
-		}
-	}
-
-	#pragma omp for
+void swapGrid(uint32_t &r, uint32_t &c, float ** grid, float ** tmp) {
 	for (uint32_t i = 1; i < r-1; i++) {
 		for (uint32_t j = 1; j < c-1; j++) {
 			grid[i][j] = tmp[i][j];
